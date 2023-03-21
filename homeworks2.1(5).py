e939e97fcda85643da9264b2968dbdb75f8e7806
rufoo7 <quliyevrufet503@gmail.com>
2023-03-21 05:02:36 -0700

    Homeworks 2.1

---
+++ homeworks2.1(5).py
@@ -0,0 +1,19 @@
+# İstifadəçi ID nömrəsini daxil edəcək. ID nömrə10 simvoldan ibarət olmalı,
+# Bunun ilk 3 hərfi hər hansı bir ölkənin qısaltması olmalı (məs, AZE, TUR, USA),
+# Həmin qısaltmalar böyük hərflə yazılan ingilis şriftləri olmalı,
+# Daha sonrakı 7 character isə ancaq rəqəmlərdən ibarət olmalıdır.
+id = input('ID-nizi daxil edin:')
+print(id[:3])
+if len(id) == 10 :
+    if id.isascii():
+        if id[:3].isupper():
+            if id.isalnum():
+                print("Daxil etdiyiniz sifre dogrudur")
+            else :
+                print("ID-niz yalniz herf ve reqemlerden ibaret olmalidir")
+        else :
+            print("Herfler yalniz boyuk yazilmalidir")
+    else :
+        print("ID-niz yalniz ingilis herflerinden ibaret ola biler")
+else : 
+    print("ID-niz yalniz 10 simvoldan ibaret ola biler")
